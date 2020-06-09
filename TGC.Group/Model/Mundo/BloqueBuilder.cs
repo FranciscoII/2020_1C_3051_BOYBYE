b06da306f7f1f3cc77f87923c88ffce3c9766c4c
FranciscoII <francortez993@gmail.com>
2020-06-09 18:48:24 -0300

    Fix #26; Good news Gordon!

--- TGC.Group/Model/Mundo/BloqueBuilder.cs
+++ TGC.Group/Model/Mundo/BloqueBuilder.cs
@@ -13,6 +13,7 @@ namespace TGC.Group.Model
         private String mediaDir;
         private TGCVector3 posicion;
         private String nombreMapa;
+        private List<TGCVector3> posicionOriginalTorretas;
         private List<TGCVector3> posicionesTorretas;
         private Nave nave;
         public BloqueBuilder(string mediaDir, TGCVector3 posicionInicial, String nombreMapa, List<TGCVector3> posiciones, Nave nave)
@@ -21,6 +22,7 @@ namespace TGC.Group.Model
             this.posicion = posicionInicial;
             this.nombreMapa = nombreMapa;
             this.posicionesTorretas = posiciones;
+            this.posicionOriginalTorretas = posiciones;
             this.nave = nave;
         }
 
@@ -33,7 +35,13 @@ namespace TGC.Group.Model
         {
             this.posicion = nuevaPosicion;
             List<TGCVector3> nuevasPosiciones = new List<TGCVector3>();
-            posicionesTorretas.ForEach(delegate (TGCVector3 pos) {pos.Z += nuevaPosicion.Z;nuevasPosiciones.Add(new TGCVector3(pos)); });
+            float nuevoZ;
+            //posicionesTorretas.ForEach(delegate (TGCVector3 pos) {pos.Z += nuevaPosicion.Z;nuevasPosiciones.Add(new TGCVector3(pos)); });
+            for(int i = 0; i < posicionesTorretas.Count; i++)
+            {
+                nuevoZ = nuevaPosicion.Z + posicionOriginalTorretas[i].Z - 1000f;
+                nuevasPosiciones.Add(new TGCVector3(posicionesTorretas[i].X, posicionesTorretas[i].Y,nuevoZ));
+            }
             this.posicionesTorretas = nuevasPosiciones;
         }
         public TGCVector3 getPosicion()
